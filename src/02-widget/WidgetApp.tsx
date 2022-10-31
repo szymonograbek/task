097a9d1cf76d6f042cd944005504140b5d332496
Marcin Lasak <lasakmarcin90@gmail.com>
2022-10-31 19:11:10 +0100

    feat: Widget task setup (#4)

--- src/02-widget/WidgetApp.tsx
+++ src/02-widget/WidgetApp.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+
+import "./widget.css";
 
 export const WidgetApp = () => {
+  const [width, setWidth] = useState<number | null>(null);
+  const iframeContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const measurements = iframeContainer.current?.getBoundingClientRect();
+    if (measurements) {
+      setWidth(measurements.width);
+    }
+  }, [setWidth]);
+
   return (
-    <div>
+    <div className="widget">
       <h1>App content</h1>
       <p>Check out our latest podcast</p>
-      <iframe src="/iframe" />
+      <div
+        style={{
+          width: "100%",
+          overflow: "hidden",
+        }}
+        ref={iframeContainer}
+      >
+        {width && (
+          <iframe
+            height="117px"
+            width={width}
+            src="/iframe"
+            style={{ border: 0 }}
+          />
+        )}
+      </div>
     </div>
   );
 };

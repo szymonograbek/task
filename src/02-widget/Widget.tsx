2ae36cc7b5f671083e22008fcf58aa0866582c6a
Szymon Ograbek <szymon.ograbek@monterail.com>
2023-10-25 08:14:06 +0200

    fix: remove min-height

--- src/02-widget/Widget.tsx
+++ src/02-widget/Widget.tsx
@@ -4,8 +4,7 @@ import { useIframeSize } from './useIframeSize'
 const widgetResizeMediaQuery = matchMedia('(max-width: 680px)')
 
 // In this task I'm not sure if I understook the flicker correctly, as at first I've understood that intially on render the iframe should already have some initial sizes
-// i.e. 117px on the wider screen and 154px on the narrow screen, but then I read that it's about visibly changing height/width so I changed the code to that approach
-// to prevent layout shift with current text I've used min-height in CSS, but if the content is more dynamic in height I would be looking for another solution
+// i.e. 117px on the wider screen and 154px on the narrow screen, but then I read that it's about visibly changing height/width so I changed the code to that approach, but the iframe still goes from 0px to whatever dynamic height it has
 
 export const Widget = () => {
   const { iframeContainerRef, iframeRef, iframeSize } = useIframeSize({
